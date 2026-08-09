f38666d7ca825f6a7357374cc8ee89299de6ab4b
LuoJuly <luojuly517@qq.com>
2026-08-09 19:13:35 +0800

    Update twrp_xpeng.mk

--- twrp_xpeng.mk
+++ twrp_xpeng.mk
@@ -7,19 +7,28 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+#$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from xpeng device
 $(call inherit-product, device/motorola/xpeng/device.mk)
 
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+#$(call inherit-product, vendor/omni/config/gsm.mk)
+
 PRODUCT_DEVICE := xpeng
-PRODUCT_NAME := omni_xpeng
+PRODUCT_NAME := twrp_xpeng
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := XT2175-2
 PRODUCT_MANUFACTURER := motorola
+
+# enable the FRP addon
+OF_ENABLE_FRP_ADDON := 1
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
